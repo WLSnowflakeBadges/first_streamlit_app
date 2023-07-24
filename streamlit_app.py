df3b3526710f432729a477fbc300bf3a6fe4cf33
WLSnowflakeBadges <140390265+WLSnowflakeBadges@users.noreply.github.com>
2023-07-24 15:27:40 +0100

    Update streamlit_app.py
    Adding structure.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@ import pandas
 import requests
 import streamlit
 import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My Parents New Healthy Diner')
 
@@ -24,6 +25,8 @@ streamlit.write('The user entered ', fruit_choice)
 fruityvice_response = requests.get(f"https://fruityvice.com/api/fruit/{fruit_choice}")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
+
+streamlit.stop()
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
 my_cur = my_cnx.cursor()
